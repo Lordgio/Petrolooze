7d2e0f4b513c554169b0bdd5e958fe69a5507a70
Jorge Roldan <jorge.roldan1@vodafone.com>
2020-10-14 16:49:53 +0200

    Added project skeleton

---
+++ app/src/main/java/dev/xxxxx/petrolooze/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.xxxxx.petrolooze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
